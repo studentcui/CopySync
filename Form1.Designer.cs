54e43d483ba51c986a0c6b2291b0cc2d16e5f116
cuiyao <cuiyao@clife.vip>
2024-03-13 03:43:10 +0800

    first version

---
+++ Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace CopySync
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(57, 34);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(205, 199);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(55, 269);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "服务器启动失败";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(55, 311);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "手机已断开！";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(306, 477);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "CopySync";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Drawing;
+using System.Net.Sockets;
+using System.Net;
+using System.Windows.Forms;
+using QRCoder;
+using System.Runtime.InteropServices;
+using SuperSocket.WebSocket;
+using SuperSocket.SocketBase.Config;
+using SuperSocket.SocketBase;
+
+
+
+namespace CopySync
+{
+    public partial class Form1 : Form
+    {
+
+        private WebSocketServer websocketServer;
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        private static extern IntPtr SetClipboardViewer(IntPtr hWndNewViewer);
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        private static extern bool ChangeClipboardChain(IntPtr hWndRemove, IntPtr hWndNewNext);
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        private static extern int SendMessage(IntPtr hWnd, int wMsg, IntPtr wParam, IntPtr lParam);
+        private const int WM_DRAWCLIPBOARD = 0x308;
+        private IntPtr _nextClipboardViewer;
+
+        public Form1()
+        {
+            InitializeComponent();
+            _nextClipboardViewer = (IntPtr)SetClipboardViewer(this.Handle);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Console.WriteLine("qidong!");
+            // 配置WebSocket服务器
+            var serverConfig = new ServerConfig
+            {
+                Port = 10000,
+                Ip = "Any",
+                MaxConnectionNumber = 100,
+                Mode = SocketMode.Tcp
+            };
+            // 创建WebSocket服务器实例
+            websocketServer = new WebSocketServer();
+            // 初始化服务器
+            if (!websocketServer.Setup(serverConfig))
+            {
+                MessageBox.Show("WebSocket服务器初始化失败！");
+                return;
+            }
+            // 为服务器设置连接事件处理程序
+            websocketServer.NewSessionConnected += WebSocketServer_NewSessionConnected;
+            websocketServer.SessionClosed += WebSocketServer_SessionClosed;
+            // 启动服务器
+            if (!websocketServer.Start())
+            {
+                MessageBox.Show("WebSocket服务器启动失败！");
+            }
+            else
+            {
+                //MessageBox.Show("WebSocket服务器启动成功！");
+                label1.Text = "服务器已启动！";
+            }
+
+            // 生成WebSocket服务器的URL
+            string wsUrl = "ws://" + GetLocalIPAddress() + ":10000/clipboard";
+
+            // 创建二维码
+            CreateQRCode(wsUrl);
+        }
+        private void WebSocketServer_NewSessionConnected(WebSocketSession session)
+        {
+            // 当有客户端连接时，打印日志
+            // MessageBox.Show("新客户端已连接！");
+            label2.Invoke((MethodInvoker)delegate {
+                label2.Text = "手机已连接！";
+            });
+        }
+        private void WebSocketServer_SessionClosed(WebSocketSession session, SuperSocket.SocketBase.CloseReason value)
+        {
+            // 当客户端断开连接时，打印日志
+            // MessageBox.Show("客户端已断开连接！");
+            label2.Invoke((MethodInvoker)delegate {
+                label2.Text = "手机已断开！";
+            });
+        }
+
+
+        // 获取本机局域网IP地址
+        private string GetLocalIPAddress()
+        {
+            var host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (var ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    return ip.ToString();
+                }
+            }
+            throw new Exception("No network adapters with an IPv4 address in the system!");
+        }
+        private void CreateQRCode(string text)
+        {
+            // 创建二维码
+            using (var qrGenerator = new QRCodeGenerator())
+            using (var qrCodeData = qrGenerator.CreateQrCode(text, QRCodeGenerator.ECCLevel.Q))
+            using (var qrCode = new QRCode(qrCodeData))
+            using (var qrCodeImage = qrCode.GetGraphic(20))
+            {
+                // 将二维码显示在PictureBox控件中
+                pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+                pictureBox1.Image = new Bitmap(qrCodeImage);
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            // 停止WebSocket服务器
+            websocketServer.Stop();
+
+            // 可以在这里添加其他清理代码，例如保存状态、释放资源等
+            Application.Exit();
+        }
+
+
+        protected override void WndProc(ref Message m)
+        {
+            switch (m.Msg)
+            {
+                case WM_DRAWCLIPBOARD:
+                    OnClipboardChanged();
+                    SendMessage(_nextClipboardViewer, m.Msg, m.WParam, m.LParam);
+                    break;
+                case 0x2 /* WM_DESTROY */:
+                    ChangeClipboardChain(this.Handle, _nextClipboardViewer);
+                    break;
+                default:
+                    base.WndProc(ref m);
+                    break;
+            }
+        }
+        private void OnClipboardChanged()
+        {
+            // 尝试读取剪切板内容
+            if (Clipboard.ContainsText())
+            {
+                string clipboardText = Clipboard.GetText();
+                Console.WriteLine("Clipboard content changed: " + clipboardText);
+                // 在这里处理剪切板内容
+                if (websocketServer != null)
+                {
+                    foreach (var session in websocketServer.GetAllSessions())
+                    {
+                        session.Send(clipboardText);
+                    }
+                }
+            }
+        }
+
+    }
+
+}
